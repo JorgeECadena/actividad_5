7cfa713fff11803800b2c5fe62f7ef650344f8bc
Jorge Cadena <a01660252@tec.mx>
2025-03-21 14:42:39 -0600

    Made numbers to appear at the center of the tile

--- memory.py
+++ memory.py
@@ -56,9 +56,9 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 8, y + 5)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 25, 'normal'))
 
     update()
     ontimer(draw, 100)
